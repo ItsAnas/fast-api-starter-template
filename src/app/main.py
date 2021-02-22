84dc1dff4980363a84af3196e650502b50ad50c4
ItsAnas <97anas@gmail.com>
2021-02-22 20:36:42 +0100

    feat: include router for auth

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
+from data.db import Base, engine
+from services.auth import auth
 
 app = FastAPI()
+
+# DB Init
+Base.metadata.create_all(engine)
+
+# Include routes
+app.include_router(
+    auth.router,
+    prefix="/user",
+    tags=["user"],
+)
+
 
 @app.get("/")
 async def root():
