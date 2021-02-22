15dd66fff3b162113b0e985aab837f69706a9fc6
ItsAnas <97anas@gmail.com>
2021-02-22 20:35:42 +0100

    feat: update run.sh script

--- src/run.sh
+++ src/run.sh
@@ -4,4 +4,5 @@ python3 -m venv env
 . env/bin/activate
 pip install -r requirements.txt
 
-uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
+cd app
+uvicorn main:app --host 0.0.0.0 --port 8000 --reload
